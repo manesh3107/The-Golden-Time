53986fdbf2c7e5d8741baaa5a4c392069ffa1fed
Manesh Prajapati <manesh3107@gmail.com>
2023-07-11 00:49:32 +0530

    Add files via upload

---
+++ gp.js
@@ -0,0 +1,98 @@
+function change(){
+
+    var a=document.getElementById("img1")
+    a.src="watch1.png"
+    
+}
+function change1(){
+
+    var a=document.getElementById("img1")
+    a.height=675
+    a.width=450
+    a.src="watch11.png"
+}
+function change2(){
+
+    var a=document.getElementById("img2")
+    // a.height=675
+    // a.width=450
+    a.src="img11.png"
+}
+
+function change3(){
+
+    var a=document.getElementById("img2")
+    // a.height=675
+    // a.width=450
+    a.src="img1111.png"
+}
+function change4(){
+
+    var a=document.getElementById("img3")
+    // a.height=675
+    // a.width=450
+    a.src="img33.png"
+}
+function change5(){
+
+    var a=document.getElementById("img3")
+    // a.height=675
+    // a.width=450
+    a.src="img33333.png"
+}
+function change6(){
+
+    var a=document.getElementById("img4")
+    // a.height=675
+    // a.width=450
+    a.src="img33.png"
+}
+function change7(){
+
+    var a=document.getElementById("img4")
+    // a.height=675
+    // a.width=450
+    a.src="img333.png"
+}
+function change8(){
+
+    var a=document.getElementById("img5")
+    // a.height=675
+    // a.width=450
+    a.src="img44.png"
+}
+function change9(){
+
+    var a=document.getElementById("img5")
+    // a.height=675
+    // a.width=450
+    a.src="img4444.png"
+}
+function change10(){
+
+    var a=document.getElementById("img6")
+    // a.height=675
+    // a.width=450
+    a.src="img3333.png"
+}
+function change11(){
+
+    var a=document.getElementById("img6")
+    // a.height=675
+    // a.width=450
+    a.src="img333333.png"
+}
+// function change12(){
+
+//     var a=document.getElementById("img9")
+//     // a.height=675
+//     // a.width=450
+//     a.src="img444.png"
+// }
+// function change13(){
+
+//     var a=document.getElementById("img9")
+//     // a.height=675
+//     // a.width=450
+//     a.src="img44.png"
+// }
